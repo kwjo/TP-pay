2b02f48fd48c2f20b4b38a100eb93bf3cef6cae3
Kwanyoung Joo <kwanyoung.joo@outlook.com>
2024-08-14 21:58:48 +0100

    edit shiny

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -1,41 +1,60 @@
 library(shiny)
+library(shinyWidgets)
 library(bslib)
 library(dplyr)
+library(plotly)
 library(ggplot2)
 
+# Load CSV files once
 Oriel2526 <- read.csv("ExpandedOrielDataFor2526.csv")
 Oriel2425 <- read.csv("ExpandedOrielDataFor2425.csv")
 Oriel2324 <- read.csv("ExpandedOrielDataFor2324.csv")
 
 ui <- page_sidebar(
-  # App title ----
   title = "Trainee Pharmacist Salary Distribution",
-  # Sidebar panel for inputs ----
   sidebar = sidebar(
-    # Input: Slider for the number of bins ----
     sliderInput(
       inputId = "bins",
       label = "Number of bins:",
       min = 1,
       max = 50,
       value = 10
+    ),
+    awesomeRadio(
+      inputId = "EmployerType",
+      label = "chosen employer type",
+      choices = c("All EmployerTypes", "Primary care only", "Hospital only"),
+      selected = "All EmployerTypes",
+      status = "danger"
     )
   ),
-  # Output: Histogram ----
-  plotOutput(outputId = "distPlot")
+  plotlyOutput(outputId = "distPlot")
 )
-server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    x <- Oriel2526$Salary
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-    hist(x,
-      breaks = bins, col = "#007bc2", border = "white",
-      xlab = "Salary (£)",
-      main = "Salary distribution for 2024-25 training year of Salary"
-    )
+server <- function(input, output) {
+  filteredData <- reactive({
+    if (input$EmployerType == "Primary care only") {
+      Oriel2526 %>%
+        filter(EmployerType == "Primary Care")
+    } else if (input$EmployerType == "Hospital only") {
+      Oriel2526 %>%
+        filter(EmployerType == "Hospital")
+    } else {
+      Oriel2526
+    }
+  })
+
+  output$distPlot <- renderPlotly({
+    p <- ggplot(filteredData(), aes(x = Salary)) +
+      geom_histogram(bins = input$bins, fill = "lightblue", color = "black") +
+      labs(
+        title = "Trainee Pharmacist Salary Distribution",
+        x = "Salary", y = "Count"
+      ) +
+      theme_minimal()
+
+    ggplotly(p)
   })
 }
-
 
 shinyApp(ui = ui, server = server)
